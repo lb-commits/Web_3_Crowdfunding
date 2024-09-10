3e420b4bc97096e5056fa654cef257fa78adda6b
codyhighroller <123840075+codyhighroller@users.noreply.github.com>
2024-09-10 15:50:23 +0200

    update modules

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import { ThirdwebProvider } from "thirdweb/react";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "./index.css";
+
+const activeChainId = 11155111; // Sepolia Chain ID
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThirdwebProvider>
+    <ThirdwebProvider activeChain={activeChainId}>
       <App />
     </ThirdwebProvider>
   </React.StrictMode>
